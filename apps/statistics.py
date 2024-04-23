3f43f34f4f303b5e490beb09ca5acb9ec74d30bd
Desmond Lartey <larteydesmond3@gmail.com>
2024-04-23 11:26:50 +0200

    v

--- apps/statistics.py
+++ apps/statistics.py
@@ -82,7 +82,7 @@ def merge_datasets(data1, data2):
     # For example, if 'Income' and 'Household' are from survey_data, include them explicitly
     combined_data = pd.merge(data1[['Gender', 'Agegroup', 'Ethnicity',	'Activitytype', 'Timeofday']],
                              data2[['Gender', 'Agegroup', 'Travel',	'Car', 'Income', 'Country', 'Household']],
-                             on=common_columns, how='inner')
+                             on=common_columns, how='outer')
 
     # Debugging: Print or log the structure of the merged data to verify it contains what you expect
     st.write("Combined Data Structure:", combined_data.head())
