e2eaed6baf64e8a109532c605dbc55b75c3277f9
Desmond Lartey <larteydesmond3@gmail.com>
2024-05-30 22:11:50 +0200

    v

--- pages/2_🪟_Split_Map.py
+++ pages/2_🪟_Split_Map.py
@@ -21,5 +21,6 @@ with st.expander("See source code"):
             left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020"
         )
         m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+        m.set_center(lat=12.15, lon=-68.26, zoom=12)  # Centering and zooming to Bonaire
 
 m.to_streamlit(height=700)
